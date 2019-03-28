f2b6e45ec984cc4a65b6b7ad423dd5e04b2015fa
Moise Valentin <moisevalentin98@gmail.com>
2019-03-28 02:24:06 +0200

    Am conectat aplicatia la serverul cu baza de date (doar pentru a vedea daca functioneaza);TODO: modificare ip server; adaugare text input username/password; preluare date server

---
+++ MobileBanking/MobileBanking/DatabaseConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileBanking
+{
+    public static class DatabaseConnection
+    {
+        public static DataTable ExecSp(string spName, List<SqlParameter> sqlParams = null)
+        {
+            string strConnect = @"SERVER=192.168.0.103\SQLEXPRESS;DATABASE=MobileBanking;UID=user;PASSWORD=abc555abc;Trusted_Connection=no;";
+            SqlConnection connection = new SqlConnection();
+            DataTable daTable = new DataTable();
+            try
+            {
+                connection = new SqlConnection(strConnect);
+                connection.Open();
+                SqlCommand command = new SqlCommand(spName, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(sqlParams.ToArray());
+                SqlCommand command2 = connection.CreateCommand();
+                SqlDataReader dataReader = command.ExecuteReader();
+                daTable.Load(dataReader);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return daTable;
+        }
+    }
+}
--- MobileBanking/MobileBanking/MainActivity.cs
+++ MobileBanking/MobileBanking/MainActivity.cs
@@ -1,9 +1,17 @@
-﻿using Android.App;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
-
+using Xamarin.Forms;
 namespace MobileBanking
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
@@ -14,6 +22,21 @@ namespace MobileBanking
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Android.Widget.Button connectButton = FindViewById<Android.Widget.Button>(Resource.Id.button1);
+            var txt = FindViewById<TextView>(Resource.Id.textview);
+            connectButton.Click += (e, o) =>
+            {
+                List<SqlParameter> sqlParameters = new List<SqlParameter>();
+                sqlParameters.Add(new SqlParameter("Username", "a"));
+                sqlParameters.Add(new SqlParameter("Password", "abc"));
+
+                DataTable loginResults = DatabaseConnection.ExecSp("ValidateLogin", sqlParameters);
+                if(loginResults.Rows.Count == 1)
+                {
+                    string user = loginResults.Rows[0]["Username"].ToString();
+                    txt.Text = "OMG MERGE";
+                }
+            };
         }
     }
 }
